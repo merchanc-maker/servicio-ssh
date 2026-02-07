b6a1a223b0359d7b8d7fd11596a415d4c9388aed
Carlos <carlos.merchanfabian@gmail.com>
2026-02-07 21:15:53 +0100

    Script de automatización de servicio SSH - Añado Menu

--- gestion_ssh.sh
+++ gestion_ssh.sh
@@ -20,6 +20,26 @@ mostrar_info_sistema() {
     echo "============================================================"
 }
 
-# Llamada inicial para probar
-clear
-mostrar_info_sistema
+# --- MENÚ PRINCIPAL ---
+while true; do
+    clear
+    mostrar_info_sistema
+    echo " 1) INSTALACIÓN"
+    echo " 2) PUESTA EN MARCHA"
+    echo " 3) PARADA"
+    echo " 4) CONSULTAR LOGS"
+    echo " 5) EDITAR CONFIGURACIÓN"
+    echo " 6) ELIMINAR SERVICIO"
+    echo " 0) SALIR"
+    read -p "Seleccione: " opcion
+    case $opcion in
+        1)  ;;
+        2)  ;;
+        3)  ;;
+        4)  ;;
+        5)  ;;
+        6)  ;;
+        0) exit 0 ;;
+    esac
+done
+
