e2eb379bf55bd9b5d5716af6a3f00f4dbfdb6cf6
Carlos <carlos.merchanfabian@gmail.com>
2026-02-07 21:45:19 +0100

    Script de automatización de servicio SSH - Añado submenú y la instalación de servicio

--- gestion_ssh.sh
+++ gestion_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Función para obtener los datos de red
+# Función para obtener los datos de red (CABEZERA)
 mostrar_info_sistema() {
     # Extraemos la IP de la interfaz enp0s3
     IP_LOCAL=$(ip -4 addr show enp0s3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
@@ -20,6 +20,23 @@ mostrar_info_sistema() {
     echo "============================================================"
 }
 
+# --- FUNCION DE INSTALACIÓN  ---
+instalacion() {
+    echo "--- Menú de Instalación ---"
+    echo "1) Manual (Paquete) | 2) Docker"
+    read -p "Opción: " metodo
+    if [ "$metodo" == "1" ]; then
+        sudo apt update && sudo apt install openssh-server -y
+    else
+        if ! command -v docker &> /dev/null; then
+	    echo "Instalando Docker en el host..."
+            sudo apt update && sudo apt install docker.io -y
+        fi
+        echo "Docker listo."
+	read -p "Presiona Enter para continuar..."
+    fi
+}
+
 # --- MENÚ PRINCIPAL ---
 while true; do
     clear
@@ -33,7 +50,7 @@ while true; do
     echo " 0) SALIR"
     read -p "Seleccione: " opcion
     case $opcion in
-        1)  ;;
+        1)  instalacion ;;
         2)  ;;
         3)  ;;
         4)  ;;
